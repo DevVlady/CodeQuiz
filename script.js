341a622f12845ef0baf95bfded830782f335397c
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-10-29 21:59:11 -0500

    Just finished writing my psuedocode in the script file.

---
+++ script.js
@@ -0,0 +1,21 @@
+//Create a variable for countdown timer
+//Create a variable to keep track of the leaderboard
+//Click START to begin quiz
+//Once the start button is clicked the timer will begin and the first question will be prompted
+//Each page will have a heading which contains a the question
+//Each page will have 4 buttons which will contain answers to the question asked
+//If the wrong answer is selected then 10 seconds will be deducted from the countdown timer
+//Each correct answer will add 10 points to the users scoresheet
+//Right or wrong alert will be displayed when the user selects an answer
+//A loop will be created where this will happen 5 times
+//On the final page text will be desplayed letting the user know the quiz is complete
+//On the 2nd to last page the user will have a screen displayed letting the user know the quiz is complete
+//The user will have an input provided where they will be asked to input their initials
+//Once the user neters their initials they will have a submit button they will need to click to continue
+//The timer will be stopped on the 2nd to last page
+//Once the user hits submit button they will be taken to the next page which will display the scores & initials of the the users
+//This page will only display the text "Lederboard"
+//The user will have 2 buttons they can click on
+//One of the buttons is "GO BACK" which will take them to the main page
+//The other button they will have as an option to click on is "CLEAR LEADERBOARD"
+//Clicking on the "LEADERBOARD" will erase all the users scores & and their initials from the leaderboard
